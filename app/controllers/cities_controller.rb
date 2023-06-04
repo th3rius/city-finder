acb60ee969dac4c2e449ee16dab51dd72916fc89
Gabriel de Oliveira <gabrielhk3@gmail.com>
2023-06-04 20:13:24 -0300

    add the ability to filter by state

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,6 +2,13 @@
 
 class CitiesController < ApplicationController
   def index
-    @cities = City.search_by_name(params[:query]).with_pg_search_highlight
+    @state_options = State.order(:name)
+                          .pluck(:name, :code)
+                          .unshift(['None', nil])
+
+    @cities = City.joins(:state)
+                  .search_by_name(params[:query])
+                  .with_pg_search_highlight
+    @cities = @cities.where(states: { code: params[:state] }) if params[:state].present?
   end
 end
